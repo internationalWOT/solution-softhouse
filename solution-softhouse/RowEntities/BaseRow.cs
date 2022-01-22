c725cf5e4bf2aa8ae2fd54066bee9b0f5004445c
Oscar Nilsson <oscar.nilsson1991@gmail.com>
2022-01-22 20:27:24 +0100

    fix: added comments

--- solution-softhouse/RowEntities/BaseRow.cs
+++ solution-softhouse/RowEntities/BaseRow.cs
@@ -3,8 +3,14 @@ using solution_softhouse.Interfaces.RowEntities;
 
 namespace solution_softhouse.RowEntities
 {
+
     public class BaseRow : IBaseRow
     {
+        /*
+         * Organizing the rowtypes by elementtype. not used in this implementation
+         * but can be used in future ones where you want more complex behaviour based on
+         * what type you are using.
+         */
         public XmlElementType xmlElementType { get; set; } = XmlElementType.xmlRootElement;
     }
 }
